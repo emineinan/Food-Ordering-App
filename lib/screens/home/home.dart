f22db64fef1a70db314c9c433ffc16e09ee0f402
Emine İnan <emineinanx@gmail.com>
2020-11-23 19:45:14 +0300

    buildHomeAppBar method added.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:food_app/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: buildHomeAppBar(context),
+    );
+  }
+
+  AppBar buildHomeAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/menu.svg"), onPressed: () {}),
+      title: RichText(
+          text: TextSpan(
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6
+                  .copyWith(fontWeight: FontWeight.bold),
+              children: [
+            TextSpan(
+              text: "Punk",
+              style: TextStyle(color: ksecondaryColor),
+            ),
+            TextSpan(
+              text: "Food",
+              style: TextStyle(color: ksecondaryColor),
+            )
+          ])),
+      actions: <Widget>[
+        IconButton(icon: SvgPicture.asset("assetName"), onPressed: () {})
+      ],
+    );
   }
 }
